54ff11300fd1083b1c638421d2fc1f02556bc6b8
estermabel <estermabel2@gmail.com>
2021-07-11 19:28:18 -0300

    icon changed

--- lib/generated/intl/messages_pt.dart
+++ lib/generated/intl/messages_pt.dart
@@ -7,7 +7,7 @@
 // ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
-// ignore_for_file:unused_import, file_names
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
 
 import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
@@ -19,18 +19,22 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'pt';
 
-  final Map<String, dynamic> messages = _notInlinedMessages(_notInlinedMessages);
-  static _notInlinedMessages(_) => <String, Function> {
-    "appTitle" : MessageLookupByLibrary.simpleMessage("Is It Safe?"),
-    "textLogin" : MessageLookupByLibrary.simpleMessage("Login"),
-    "textOnBoarding1_0" : MessageLookupByLibrary.simpleMessage("Foco em"),
-    "textOnBoarding1_1" : MessageLookupByLibrary.simpleMessage("segurança"),
-    "textOnBoarding1_2" : MessageLookupByLibrary.simpleMessage("e"),
-    "textOnBoarding1_3" : MessageLookupByLibrary.simpleMessage("comunidade"),
-    "textOnBoarding2_0" : MessageLookupByLibrary.simpleMessage("Saiba onde frequentar com"),
-    "textOnBoarding2_1" : MessageLookupByLibrary.simpleMessage("segurança"),
-    "textOnBoarding3_0" : MessageLookupByLibrary.simpleMessage("Descreva suas"),
-    "textOnBoarding3_1" : MessageLookupByLibrary.simpleMessage("experiências"),
-    "textOnBoarding3_2" : MessageLookupByLibrary.simpleMessage("e avalie locais")
-  };
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "appTitle": MessageLookupByLibrary.simpleMessage("Is It Safe?"),
+        "textLogin": MessageLookupByLibrary.simpleMessage("Login"),
+        "textOnBoarding1_0": MessageLookupByLibrary.simpleMessage("Foco em"),
+        "textOnBoarding1_1": MessageLookupByLibrary.simpleMessage("segurança"),
+        "textOnBoarding1_2": MessageLookupByLibrary.simpleMessage("e"),
+        "textOnBoarding1_3": MessageLookupByLibrary.simpleMessage("comunidade"),
+        "textOnBoarding2_0":
+            MessageLookupByLibrary.simpleMessage("Saiba onde frequentar com"),
+        "textOnBoarding2_1": MessageLookupByLibrary.simpleMessage("segurança"),
+        "textOnBoarding3_0":
+            MessageLookupByLibrary.simpleMessage("Descreva suas"),
+        "textOnBoarding3_1":
+            MessageLookupByLibrary.simpleMessage("experiências"),
+        "textOnBoarding3_2":
+            MessageLookupByLibrary.simpleMessage("e avalie locais")
+      };
 }
--- lib/pages/on_boarding_page.dart
+++ lib/pages/on_boarding_page.dart
@@ -61,21 +61,21 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
       text: TextSpan(
         children: [
           TextSpan(
-            text: "${S.of(context)!.textOnBoarding1_0} ",
+            text: "${S.of(context).textOnBoarding1_0} ",
             style: Theme.of(context).textTheme.headline4,
           ),
           TextSpan(
-            text: "${S.of(context)!.textOnBoarding1_1} ",
+            text: "${S.of(context).textOnBoarding1_1} ",
             style: Theme.of(context).textTheme.headline4!.copyWith(
                   fontWeight: FontWeight.bold,
                 ),
           ),
           TextSpan(
-            text: "${S.of(context)!.textOnBoarding1_2} ",
+            text: "${S.of(context).textOnBoarding1_2} ",
             style: Theme.of(context).textTheme.headline4,
           ),
           TextSpan(
-            text: "${S.of(context)!.textOnBoarding1_3}.",
+            text: "${S.of(context).textOnBoarding1_3}.",
             style: Theme.of(context).textTheme.headline4!.copyWith(
                   fontWeight: FontWeight.bold,
                 ),
@@ -90,11 +90,11 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
       text: TextSpan(
         children: [
           TextSpan(
-            text: "${S.of(context)!.textOnBoarding2_0} ",
+            text: "${S.of(context).textOnBoarding2_0} ",
             style: Theme.of(context).textTheme.headline4,
           ),
           TextSpan(
-            text: "${S.of(context)!.textOnBoarding2_1}.",
+            text: "${S.of(context).textOnBoarding2_1}.",
             style: Theme.of(context).textTheme.headline4!.copyWith(
                   fontWeight: FontWeight.bold,
                 ),
@@ -109,17 +109,17 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
       text: TextSpan(
         children: [
           TextSpan(
-            text: "${S.of(context)!.textOnBoarding3_0} ",
+            text: "${S.of(context).textOnBoarding3_0} ",
             style: Theme.of(context).textTheme.headline4,
           ),
           TextSpan(
-            text: "${S.of(context)!.textOnBoarding3_1} ",
+            text: "${S.of(context).textOnBoarding3_1} ",
             style: Theme.of(context).textTheme.headline4!.copyWith(
                   fontWeight: FontWeight.bold,
                 ),
           ),
           TextSpan(
-            text: "${S.of(context)!.textOnBoarding3_2}.",
+            text: "${S.of(context).textOnBoarding3_2}.",
             style: Theme.of(context).textTheme.headline4,
           ),
         ],
